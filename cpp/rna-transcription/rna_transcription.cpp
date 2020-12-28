9b552269ee008856d0b21604a6e4574c8de41d4b
mehdi-fakihani <mehdi.fakihani@outlook.com>
2020-12-28 09:30:12 +0100

    Add RNA Transcription

--- cpp/rna-transcription/rna_transcription.cpp
+++ cpp/rna-transcription/rna_transcription.cpp
@@ -1,5 +1,31 @@
 #include "rna_transcription.h"
 
+#include <algorithm>
+
 namespace rna_transcription {
+
+    std::string to_rna(const std::string& dna)
+    {
+        std::string rna {dna};
+        std::for_each(rna.begin(),rna.end(),[](char& nucleotide){nucleotide = to_rna(nucleotide);});
+        return rna;
+    }
+
+    char to_rna(const char &nucleotide)
+    {
+        switch(nucleotide)
+        {
+            case 'A':
+                return 'U';
+            case 'T':
+                return 'A';
+            case 'C':
+                return 'G';
+            case 'G':
+                return 'C';
+            default:
+                return '?';
+        }
+    }
 
 }  // namespace rna_transcription
--- cpp/rna-transcription/rna_transcription.h
+++ cpp/rna-transcription/rna_transcription.h
@@ -1,7 +1,12 @@
 #if !defined(RNA_TRANSCRIPTION_H)
 #define RNA_TRANSCRIPTION_H
 
+#include <string>
+
 namespace rna_transcription {
+
+    std::string to_rna(const std::string& dna);
+    char to_rna(const char& nucleotide);
 
 }  // namespace rna_transcription
 
