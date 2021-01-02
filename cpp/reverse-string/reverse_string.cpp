e56ab1a6930a41436499dc97930b67286e25a8cb
mehdi-fakihani <mehdi.fakihani@outlook.com>
2021-01-02 01:35:23 +0100

    Update Reverse String solution
    According to Exercism mentor's comment on my solution, I publish this update

--- cpp/reverse-string/reverse_string.cpp
+++ cpp/reverse-string/reverse_string.cpp
@@ -1,13 +1,10 @@
 #include "reverse_string.h"
 
-#include <algorithm>
-
 namespace reverse_string {
 
-    std::string reverse_string(const std::string &string)
+    std::string reverse_string(const std::string& string)
     {
-        std::string copy {string};
-        std::reverse_copy(string.begin(),string.end(),copy.begin());
+        std::string copy {string.rbegin(), string.rend()};
         return copy;
     }
 
