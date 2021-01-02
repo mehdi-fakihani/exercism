e4c24c585e7da64c33b9e74d07ecd6a6e2ac9dfc
mehdi-fakihani <mehdi.fakihani@outlook.com>
2021-01-02 01:23:35 +0100

    [C++] Add Say

---
+++ cpp/say/say.h
@@ -0,0 +1,12 @@
+#if !defined(SAY_H)
+#define SAY_H
+
+#include <string>
+
+namespace say {
+
+    std::string in_english(const unsigned long long int& number);
+
+}  // namespace say
+
+#endif // SAY_H
